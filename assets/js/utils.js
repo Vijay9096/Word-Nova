12501cf8255cdb50fef16d475b3c9fc58c230eed
Vijay9096 <mulchandanivijay58@gmail.com>
2026-05-08 21:43:47 +0530

    update

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -5,6 +5,18 @@ function waitForButtonActive(callback, delay = 120) {
   }, delay);
 }
 
-
-
-
+let progress = 0;
+const bar = document.getElementById("loading-bar");
+const text = document.getElementById("loading-text");
+const splash = document.getElementById("splash-screen");
+const loadingInterval = setInterval(() => {
+    progress++;
+    bar.style.width = progress + "%";
+    text.innerText = "Loading... " + progress + "%";
+    if(progress >= 100){
+        clearInterval(loadingInterval);
+        setTimeout(() => {
+            splash.style.display = "none";
+        }, 300);
+    }
+}, 25);
